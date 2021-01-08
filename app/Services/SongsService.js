e65d15cc4faa30c4a7d0f4309a8854eba1e96667
Chase16Rogers <chase16rogers@gmail.com>
2021-01-07 17:13:09 -0700

    Made Project

--- app/Services/SongsService.js
+++ app/Services/SongsService.js
@@ -3,11 +3,24 @@ import Song from "../Models/Song.js";
 import { sandBoxApi } from "./AxiosService.js";
 
 class SongsService {
+  drawAudio(id) {
+    let music = ProxyState.playlist.find(p => p.trackid == id)
+    document.getElementById("audio").innerHTML = music.AudioTag
+  }
+  drawActiveSong(id) {
+    let active = ProxyState.songs.find(s => s.trackid == id)
+    ProxyState.activeSong = active
+  }
+  drawActivePlaylist(id) {
+    let active = ProxyState.playlist.find(s => s.trackid == id)
+    ProxyState.activeSong = active
+  }
   /**
    * Takes in a search query and retrieves the results that will be put in the store
    * @param {string} query
    */
   getMusicByQuery(query) {
+    ProxyState.songs = []
     //NOTE You will not need to change this method
     let url = "https://itunes.apple.com/search?callback=?&term=" + query;
     // @ts-ignore
@@ -25,6 +38,10 @@ class SongsService {
    */
   async getMySongs() {
     //TODO What are you going to do with this result
+    let res = await sandBoxApi.get("")
+    ProxyState.playlist = res.data.map(p => new Song(p))
+    console.log(ProxyState.playlist)
+    console.log(res.data)
   }
 
   /**
@@ -32,9 +49,13 @@ class SongsService {
    * Afterwords it will update the store to reflect saved info
    * @param {string} id
    */
-  addSong(id) {
+  async addSong(id) {
     //TODO you only have an id, you will need to find it in the store before you can post it
     //TODO After posting it what should you do?
+
+    let song = ProxyState.songs.find(s => s.trackid == id)
+    let res = await sandBoxApi.post("", song)
+    ProxyState.playlist = [...ProxyState.playlist, new Song(res.data)]
   }
 
   /**
@@ -42,8 +63,10 @@ class SongsService {
    * Afterwords it will update the store to reflect saved info
    * @param {string} id
    */
-  removeSong(id) {
+  async removeSong(id) {
     //TODO Send the id to be deleted from the server then update the store
+    await sandBoxApi.delete(id)
+    ProxyState.playlist = ProxyState.playlist.filter(p => p.trackid != id)
   }
 }
 
